6ac91eb1cb60bf2fa832d347d823f1e82069c44c
hiroshi-kato <hiroshikato1208@gmail.com>
2024-07-19 09:31:05 +0900

    refactor: Update Template component to use inline styles

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,11 +1,37 @@
 import React from "https://esm.sh/react@18.2.0";
-import './Template.css';
 
 export const Template = ({ imageUrl }: { imageUrl: string }) => (
-  <div className="template-container">
-    <img src={imageUrl} alt="base image" className="template-image" />
-    <div className="template-overlay">
-      <p className="template-text">LGTM</p>
+  <div
+    style={{ position: "relative", width: 800, height: 600, display: "flex" }}
+  >
+    <img
+      src={imageUrl}
+      alt="base image"
+      style={{ width: 800, height: 600, objectFit: "contain" }}
+    />
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p
+        style={{
+          color: "white",
+          fontSize: 100,
+          fontWeight: "bold",
+          letterSpacing: "1.5rem",
+          textShadow: "2px 2px 3px #808080"
+        }}
+      >
+        LGTM
+      </p>
     </div>
   </div>
 );
