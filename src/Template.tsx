7987e458b89777d63d0bfe219aae897fc0cde761
hiroshi-kato <hiroshikato1208@gmail.com>
2024-07-19 09:26:12 +0900

    ♻️ Refactor style

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,36 +1,11 @@
 import React from "https://esm.sh/react@18.2.0";
+import './Template.css';
 
 export const Template = ({ imageUrl }: { imageUrl: string }) => (
-  <div
-    style={{ position: "relative", width: 800, height: 600, display: "flex" }}
-  >
-    <img
-      src={imageUrl}
-      alt="base image"
-      style={{ width: 800, height: 600, objectFit: "contain" }}
-    />
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <p
-        style={{
-          color: "white",
-          fontSize: 100,
-          fontWeight: "bold",
-          letterSpacing: "1.5rem",
-        }}
-      >
-        LGTM
-      </p>
+  <div className="template-container">
+    <img src={imageUrl} alt="base image" className="template-image" />
+    <div className="template-overlay">
+      <p className="template-text">LGTM</p>
     </div>
   </div>
 );
